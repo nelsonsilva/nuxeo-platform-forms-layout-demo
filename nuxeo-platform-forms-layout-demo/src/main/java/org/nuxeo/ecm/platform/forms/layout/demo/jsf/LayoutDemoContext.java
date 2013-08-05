08a0555d57411d7b35badba165ae6e3a8244ed19
Guillaume Renard <grenard@nuxeo.com>
2013-08-05 15:03:39 +0200

    NXP-12018: add specific field in layout demo schema for select2 and change the displayed icon

--- nuxeo-platform-forms-layout-demo/src/main/java/org/nuxeo/ecm/platform/forms/layout/demo/jsf/LayoutDemoContext.java
+++ nuxeo-platform-forms-layout-demo/src/main/java/org/nuxeo/ecm/platform/forms/layout/demo/jsf/LayoutDemoContext.java
@@ -174,6 +174,7 @@ public class LayoutDemoContext implements Serializable {
         // fill all fields used in preview
         if (index <= 1) {
             doc.setPropertyValue("lds:textField", "Some sample text");
+            doc.setPropertyValue("lds:anotherTextField", "");
             doc.setPropertyValue("lds:textareaField",
                     "Some sample text with\nseveral lines.");
             doc.setPropertyValue("lds:htmlField",
@@ -217,6 +218,7 @@ public class LayoutDemoContext implements Serializable {
 
         } else {
             doc.setPropertyValue("lds:textField", "Some other sample text");
+            doc.setPropertyValue("lds:anotherTextField", "");
             doc.setPropertyValue("lds:textareaField",
                     "Some other sample text with\nseveral lines.");
             doc.setPropertyValue("lds:htmlField",
